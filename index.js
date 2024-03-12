a458d77952d3a4ceb9dd0a7c4be6f7d2071748e6
Yehor <yehor12344321@gmail.com>
2024-03-12 18:47:26 +0200

    зберігає зміни

---
+++ index.js
@@ -0,0 +1,46 @@
+// for (let i = 0; i < 11; i+=1) {
+//     console.log(i); 
+// }
+
+// for (let i = 2; i < 21; i+=1) {
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }else{
+
+//     }
+// }
+
+// for (let i = 0; i < 11; i+=1) {
+//     let multiplyNum = 7 * i;
+//     console.log(multiplyNum);   
+// }
+
+// const arrOfNums = [1, 2, 3, 4, 5 ];
+
+// for(let i = 0; i <= arrOfNums.length - 1; i+=1){
+//     console.log(arrOfNums[i]);
+// }
+
+// const arrOfNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for(let i = 0; i <= arrOfNums.length - 1; i+=1){
+    
+//     if (arrOfNums[i] === 7) {
+//         break
+//     }
+//     console.log(arrOfNums[i]);
+// }
+
+// for(let i = 0; i<= 123; i+= 1 ){
+//     if(i >= 123){
+//         break
+//     }
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 20; i+= 1) {
+//     if(i % 3 === 0 ){
+//         continue
+//     }
+//     console.log(i);
+// }
